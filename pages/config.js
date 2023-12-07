45f9811aa7471b5a29037e377e3a16de7bf833ab
haikali3 <manfdvcL9@gmail.com>
2023-12-07 22:04:24 +0800

    Update package-lock.json and pages/config.js

--- pages/config.js
+++ pages/config.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables from .env file
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 'default_value';
 
 export { MONGODB_URI };
